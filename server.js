0ec82065291d4aebb6d223bb3763b2bf082bda9b
mrabanda <mrabanda@gmail.com>
2017-04-19 23:59:34 -0400

    added passport

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const express           = require('express');
 const logger            = require('morgan');
 const webpack           = require('webpack');
 const mongoose          = require('mongoose');
+const cookieParser      = require('cookie-parser');
+const bodyParser        = require('body-parser');
+const expressValidator  = require('express-validator');
+const flash             = require('connect-flash');
+const session           = require('express-session');
+const passport          = require('passport');
+const LocalStrategy     = require('LocalStrategy');
+
 // const configWebpack     = require('./webpack.config.js');
 // const webpackMiddleware = require('webpack-dev-middleware');
 
@@ -41,8 +49,53 @@ const app = express();
 // const compiler = webpack(configWebpack(env));
 
 // Middleware Setup ========================================
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use("/public", express.static(path.resolve(__dirname + "/public")));
 // app.use(webpackMiddleware(compiler));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+ 
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//Connect Flash
+app.use(flash());
+
+// Global Variables for Flash
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
 // Morgan Logger
 app.use(logger('dev'));
