3acacbe6674641c4198949c492515e2053f9af27
Justin Bellero <jpbellero@gmail.com>
2017-04-20 18:41:47 -0400

    updates webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   context: path.resolve(__dirname),
   entry: {
     main: './client/index.js',
-    vendor: ['react', 'react-dom', 'react-router'],
+    vendor: ['react', 'react-dom', 'react-router-dom'],
     style: './client/stylesheets/main.sass',
   },
   output: {
