fe1cdbd4258c20343f050d78a0434727c2bde02a
Justin Bellero <jpbellero@gmail.com>
2017-04-20 21:46:41 -0400

    base structure complete

--- client/app/Router.js
+++ client/app/Router.js
@@ -1,23 +1,11 @@
 // React Router Required Objects
-// import React from 'react';
-// const { Router, Route, hashHistory } = require('react-router');
-var React = require('react');
-var router = require("react-router");
+const React = require('react');
+const router = require("react-router");
+const Route = router.Route;
+const Router = router.Router;
+const hashHistory = router.hashHistory;
+const IndexRoute = router.IndexRoute;
 
-// Include the Route component for displaying individual routes
-var Route = router.Route;
-
-// Include the Router component to contain all our Routes
-// Here where we can pass in some configuration as props
-var Router = router.Router;
-
-// Include the hashHistory prop to handle routing client side without a server
-// https://github.com/ReactTraining/react-router/blob/master/docs/guides/Histories.md#hashhistory
-var hashHistory = router.hashHistory;
-
-// Include the IndexRoute (catch-all route)
-var IndexRoute = router.IndexRoute;
-// react-router-configs here
 import App from './App';
 
 module.exports = (
