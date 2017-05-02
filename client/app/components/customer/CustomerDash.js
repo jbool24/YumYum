d0ec3e5602d74a3132b5e1380d2c110ff10bc0cb
Justin Bellero <jpbellero@gmail.com>
2017-05-01 20:55:20 -0400

    fixed customer header based on page context

--- client/app/components/customer/CustomerDash.js
+++ client/app/components/customer/CustomerDash.js
@@ -6,9 +6,17 @@ const CustomerFilterHeader = require('./CustomerFilterHeader');
 const CustomerDash = React.createClass({
 
 	render: function(){
+		const hash = window.location.hash;
+		const context = hash.slice(0, -(hash.length - hash.indexOf("?")));
+
+		let header = "";
+
+		if (context === "#/customer/customer-dashboard/filter-search")
+  				header = <CustomerFilterHeader/>
+
 		return(
 			<div>
-	      <CustomerFilterHeader />
+	      { header }
 	      {this.props.children}
 			</div>
 		);
