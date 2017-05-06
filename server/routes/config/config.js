77735b84ff394553e697a4c718a44f1819cef6d2
Justin Bellero <jpbellero@gmail.com>
2017-05-06 11:21:33 -0400

    add changes for heroku

--- server/routes/config/config.js
+++ server/routes/config/config.js
@@ -2,6 +2,6 @@ module.exports = {
   'googleAuth': {
     'clientID': '188696954394-mpr4mcb5prpku3edvd733atu3uc58pcn.apps.googleusercontent.com',
     'clientSecret': 'OG-IT6g0gdbc4-QDLOd7p9-G',
-    'callbackURL': 'http://foodfriends-app.herokuapp.com/auth/google/callback'
+    'callbackURL': 'https://foodfriends-app.herokuapp.com/auth/google/callback'
   }
 }
