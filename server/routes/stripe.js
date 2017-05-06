db1e02f97f9070c6948a6a0a2ce07ff4fe798879
mrabanda <mrabanda@gmail.com>
2017-05-06 02:55:17 -0400

    save orders

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -5,7 +5,8 @@ const Order = require('../models/Order');
 const Customer = require('../models/Customer')
 
 module.exports = function stripeCharge(req, res) {
-  console.log(req.body);
+  // console.log(req.body);
+  // console.log(req.user);
   // Token is created using Stripe.js or Checkout!
   // Get the payment token submitted by the form:
   let {stripeToken, stripeEmail, amount} = req.body; // Using Express
@@ -23,12 +24,39 @@ module.exports = function stripeCharge(req, res) {
       customer: customer.id,
     });
   }).then(function (charge) {
+    console.log(req.session.cart.items);
     // Use and save the charge info.
-    // let order = {
-    //   customer: 
-    // }
-    let newOrder = new Order()
-    res.redirect('/home#/customer/dashboard/search')
+    let cartItemIds = Object.keys(req.session.cart.items)
+    let orderCustomer = req.user._id
+    let orderCook = req['session']['cart']['items'][cartItemIds[0]]['item.cook']
+
+    let order = {
+      id: charge.id,
+      customer: orderCustomer,
+      cook: orderCook ,
+      fooditems: cartItemIds,
+      createdOn: Date.now(),
+      orderCompleted: true,
+      lastUpdated: Date.now(),
+      orderTotal: amount/100  
+    };
+
+    let newOrder = new Order(order)
+    req.session.cart = undefined
+    newOrder.save(function (error, doc) {
+      if (error) {
+        res.send(error);
+      } else {
+        Customer.findOneAndUpdate({ "_id": orderCustomer }, { $push: { "order_history": doc._id } }, { new: true }, function (err, newdoc) {
+          if (err) {
+            res.send(err);
+          } else {
+            res.redirect('/home#/customer/dashboard/search')
+
+          }
+        })
+      }
+    })
     console.log(charge);
   });
 }
